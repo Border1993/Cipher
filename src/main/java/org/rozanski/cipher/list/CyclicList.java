72274c3530f7f1c7671d8e71cb627719c748b633
Wojciech Rozanski <rozanskiwojciech93@gmail.com>
2025-09-27 23:15:01 +0200

    Abandoned list interface

--- src/main/java/org/rozanski/cipher/list/CyclicList.java
+++ src/main/java/org/rozanski/cipher/list/CyclicList.java
@@ -1,45 +1,17 @@
 package org.rozanski.cipher.list;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-
-public class CyclicList<T> implements List<T> {
+public class CyclicList<T> {
     private int size = 0;
     private Node<T> firstElementInChain = null;
 
-    @Override
     public int size() {
         return size;
     }
 
-    @Override
     public boolean isEmpty() {
         return size == 0;
     }
 
-    @Override
-    public boolean contains(Object o) {
-        return false;
-    }
-
-    @Override
-    public Iterator<T> iterator() {
-        return null;
-    }
-
-    @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    @Override
-    public <T1> T1[] toArray(T1[] a) {
-        return null;
-    }
-
-    @Override
     public boolean add(T t) {
         final Node<T> newElement = new Node<>(t);
 
@@ -60,87 +32,11 @@ public class CyclicList<T> implements List<T> {
         return current;
     }
 
-    @Override
-    public boolean remove(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends T> c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(int index, Collection<? extends T> c) {
-        return false;
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public void clear() {
-
-    }
-
-    @Override
     public T get(int index) {
         Node<T> current = firstElementInChain;
         for (int i = 0; i < index; i++) {
             current = current.getNext();
         }
         return current.getValue();
-    }
-
-    @Override
-    public T set(int index, T element) {
-        return null;
-    }
-
-    @Override
-    public void add(int index, T element) {
-
-    }
-
-    @Override
-    public T remove(int index) {
-        return null;
-    }
-
-    @Override
-    public int indexOf(Object o) {
-        return 0;
-    }
-
-    @Override
-    public int lastIndexOf(Object o) {
-        return 0;
-    }
-
-    @Override
-    public ListIterator<T> listIterator() {
-        return null;
-    }
-
-    @Override
-    public ListIterator<T> listIterator(int index) {
-        return null;
-    }
-
-    @Override
-    public List<T> subList(int fromIndex, int toIndex) {
-        return List.of();
     }
 }
