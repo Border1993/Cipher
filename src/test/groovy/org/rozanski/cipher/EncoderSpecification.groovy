01bb642f769ce7b733acee7b5beb75f21b94f3ca
Wojciech Rozanski <rozanskiwojciech93@gmail.com>
2025-09-28 23:02:06 +0200

    Added stub tests for encoder

--- src/test/groovy/org/rozanski/cipher/EncoderSpecification.groovy
+++ src/test/groovy/org/rozanski/cipher/EncoderSpecification.groovy
@@ -2,6 +2,7 @@ package org.rozanski.cipher
 
 import org.rozanski.cipher.list.CipherCycle
 import spock.lang.Specification
+import spock.lang.Subject
 
 class EncoderSpecification extends Specification {
     def "encoder uses cipher do encode the text"() {
@@ -9,8 +10,8 @@ class EncoderSpecification extends Specification {
         def text = "aaabbc"
         def shift = 2;
         CipherCycle cipherCycle = Mock()
+        @Subject
         def encoder = new Encoder(cipherCycle)
-
 
         when:
         encoder.encode(text, shift)
@@ -19,6 +20,24 @@ class EncoderSpecification extends Specification {
         3 * cipherCycle.shift("a", shift)
         2 * cipherCycle.shift("b", shift)
         1 * cipherCycle.shift("c", shift);
+    }
+
+    def "message is encoded properly"() {
+        given:
+        def text = "aaabbc"
+        def shift = 2;
+        CipherCycle cipherCycle = Stub()
+        @Subject
+        def encoder = new Encoder(cipherCycle)
+        cipherCycle.shift("a", 2) >> "c"
+        cipherCycle.shift("b", 2) >> "d"
+        cipherCycle.shift("c", 2) >> "e"
+
+        when:
+        def encodedText = encoder.encode(text, shift)
+
+        then:
+        encodedText == "cccdde"
     }
 
 }
